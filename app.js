8b147a46ffff536c862e524e948e8dc17086f4f2
Jérémy <jeremy100401@gmail.com>
2023-11-08 23:21:25 +0100

    création de la logique de connexion à la DB

--- app.js
+++ app.js
@@ -1,9 +1,51 @@
 const express = require('express')
+const mysql = require('mysql');
+const cors = require("cors");
+let DBCONNECTION = require("./database/database");
+
 const app = express()
 const port = 3000
 
+app.use(express.json());
+app.use(
+    cors({
+        credentials: true,
+        origin: true,
+    })
+);
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(DBCONNECTION)
+})
+
+app.post('/api/try/connect', (req, res) => {
+    console.log(req.body);
+    const body = req.body
+    DBCONNECTION = body;
+    const connection = mysql.createConnection({
+        host: body.serveur,
+        user: body.user,
+        password: body.pwd,
+        database: body.db,
+        port: body.port ?? 3306
+    });
+
+    connection.connect(err => {
+        if (err) {
+            console.log('connection failed')
+            res.send({
+                success: false,
+                error: err
+            })
+        } else {
+            console.log('connection succeed')
+            res.send({
+                success: true,
+            })
+        }
+    });
+
+    connection.end();
 })
 
 app.listen(port, () => {
---
+++ database/database.js
@@ -0,0 +1,3 @@
+let DBCONNECTION = {};
+
+module.exports = DBCONNECTION;
