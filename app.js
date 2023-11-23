32cb404bbd9dfa406b63cccae1cdb85470c81837
JRobert <jeremy100401@gmail.com>
2023-11-23 10:09:32 +0100

    use port 3001

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const { expressMiddlewareRoutes } = require("./routes/app.routes");
 
 const app = express()
-const port = 3000
+const port = 3001
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
