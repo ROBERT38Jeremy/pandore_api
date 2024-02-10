66fc8aa2d0c088e21b812bc0b6a57b9a188b88a4
Jérémy <jeremy100401@gmail.com>
2024-02-10 11:48:39 +0100

    send sql error message if error

--- features/RawQuery/rawQuery.controller.js
+++ features/RawQuery/rawQuery.controller.js
@@ -1,34 +1,39 @@
 const { MysqlDB } = require("../../database/database")
 
 exports.processRawQuery = async (req, res, _) => {
-    const { database } = req.params;
-    const { request } = req.query
+    try {
+        const { database } = req.params;
+        const { request } = req.query
 
-    const DB = MysqlDB.getInstance();
-    if (DB === null || DB?.connection === null) {
-        return res.send({
-            success: false,
-            error: 'Connection failed'
+        const DB = MysqlDB.getInstance();
+        if (DB === null || DB?.connection === null) {
+            return res.send({
+                success: false,
+                error: 'Connection failed'
+            })
+        }
+        await DB.connection.query(`USE ${database};`);
+
+        const result = await new Promise((resolve, reject) => {
+            DB.connection.query(request, (err, result, fields) => {
+                if (err) {
+                    return reject({
+                        success: false,
+                        error: err
+                    });
+                } else {
+                    return resolve({
+                        success: result,
+                        database: database,
+                        request: request
+                    })
+                }
+            })
         })
+
+        return res.send(result)
+    } catch (error) {
+        console.log(error?.error?.sqlMessage ?? error)
+        return res.send({ error: error?.error?.sqlMessage ?? error })
     }
-    await DB.connection.query(`USE ${database};`);
-
-    const result = await new Promise((resolve, reject) => {
-        DB.connection.query(request, (err, result, fields) => {
-            if (err) {
-                return reject({
-                    success: false,
-                    error: err
-                });
-            } else {
-                return resolve({
-                    success: result,
-                    database: database,
-                    request: request
-                })
-            }
-        })
-    })
-
-    return res.send(result)
 }
