e1e30f2bf4246a03c1de2382f417fb1f785c46c3
Jérémy <jeremy100401@gmail.com>
2024-04-06 23:19:56 +0200

    use getDatabaseStructure in getDatabasesMenuList

--- features/DisplayDatabase/displayDatabase.controller.js
+++ features/DisplayDatabase/displayDatabase.controller.js
@@ -21,21 +21,13 @@ exports.getDatabasesMenuList = async (req, res, _) => {
         return res.send(databaseList)
     }
 
+    await DB.connection.query(`USE information_schema;`);
     for (const db of databaseList.success) {
-        await DB.connection.query(`USE information_schema;`);
+        const databaseTablesSchemas = await DisplayDatabase.getDatabaseStructure(db);
 
-        await new Promise((resolve, reject) => {
-            DB.connection.query("SELECT * FROM `TABLES` WHERE `TABLE_SCHEMA` = ?;", [ db ], (err, result, fields) => {
-                if (err) {
-                    return reject();
-                } else {
-                    databaseMenuList[db] =  result.map((table) => {
-                        return table.TABLE_NAME;
-                    });
-                    return resolve();
-                }
-            })
-        })
+        databaseMenuList[db] = await databaseTablesSchemas.success.map((table) => {
+            return table.TABLE_NAME;
+        });
     }
 
     return res.send({ success: databaseMenuList })
